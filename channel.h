8a7da88707972185552a07a04a8c408d3df3219b
Luis Pabón <lpabon@redhat.com>
2015-05-29 21:50:40 +0000

    added channels in C

---
+++ channel.h
@@ -0,0 +1,21 @@
+#ifndef _CHANNEL_H
+#define _CHANNEL_H
+
+typedef struct {
+    int socket;
+    char buf[4096];
+    int len;
+} msg_t;
+
+typedef struct {
+    pthread_cond_t send_c;
+    pthread_cond_t recv_c;
+    pthread_mutex_t m;
+    msg_t *msg;
+} channel_t;
+
+void ch_init(channel_t *ch); 
+void ch_send(channel_t *ch, msg_t *msg);
+msg_t * ch_recv(channel_t *ch);
+
+#endif
--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
+// #cgo CFLAGS: -pthread
+// #include "pthread.h"
+// #include "channel.h"
+import "C"
+
 import (
 	"fmt"
 	"sync"
 	"time"
 )
+
+func channel_send_test(c_chan *C.channel_t, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var pkt C.msg_t
+
+	for i := 0; i < 50; i++ {
+		C.ch_send(c_chan, &pkt)
+		fmt.Println("PING >>")
+		time.Sleep(time.Millisecond * time.Duration(i*2))
+	}
+}
+
+func channel_recv_test(c_chan *C.channel_t, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < 50; i++ {
+
+		C.ch_recv(c_chan)
+		fmt.Println("<< PONG")
+		time.Sleep(time.Millisecond * time.Duration(i*5))
+	}
+}
 
 func print_test(name string, wg *sync.WaitGroup) {
 	defer wg.Done()
@@ -16,8 +44,13 @@ func print_test(name string, wg *sync.WaitGroup) {
 
 func main() {
 	var wg sync.WaitGroup
+	var c_chan C.channel_t
 
-	wg.Add(2)
+	C.ch_init(&c_chan)
+
+	wg.Add(4)
+	go channel_send_test(&c_chan, &wg)
+	go channel_recv_test(&c_chan, &wg)
 	go print_test("A", &wg)
 	go print_test("B", &wg)
 
